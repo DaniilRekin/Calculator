2c915e70aab31213d3943e8074a3c44713ee9ad1
Daniil <daniilrekin86@gmail.com>
2026-08-02 10:57:30 +0300

    Убраны лишние комментарии

--- main.cpp
+++ main.cpp
@@ -2,17 +2,7 @@
 #include "import/Lexer.h"
 #include "import/Parser.h"
 #include "import/Calculator.h"
-
 #include "import/Exception.h"
-
-// Последовательность файлов
-// Utils.h
-//   Utils.cpp
-
-// Token.h
-
-// Lexer.h
-//   Lexer.cpp
 
 int main() {
   Calculator calc(std::cin, std::cout);
